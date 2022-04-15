928be31670e451dd00295dabea1d97df60b30421
Jtobyy <jtobi8161@gmail.com>
2022-04-15 01:46:07 +0100

    Doc

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,5 +6,5 @@ from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """  """
+    """ Returns a list of tuples """
     return [(i, len(i)) for i in lst]
