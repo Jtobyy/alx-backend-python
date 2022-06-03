d943a520b377513c5c246a856ed32f354bf81a3c
Jtobyy <jtobi8161@gmail.com>
2022-06-03 09:19:11 +0100

    Annotate 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ Tasks """
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> asyncio.Task:
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     '''identical to wait_n except task_wait_random is being called.'''
     ls = []
     for _ in range(n):
