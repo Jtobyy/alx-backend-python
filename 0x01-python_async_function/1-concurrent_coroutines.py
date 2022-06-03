ac83637f0ad89b2c66937a60eacbef46a5032c00
Jtobyy <jtobi8161@gmail.com>
2022-06-03 08:13:53 +0100

    Pycodestyle for 1-concurrent.....py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """ return the list of all the delays """
     ls = []
