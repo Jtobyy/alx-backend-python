ce061503d0c4a6f999f372ca04dce2d04b406a4e
Jtobyy <jtobi8161@gmail.com>
2022-06-06 23:44:21 +0100

    Annotate function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -6,7 +6,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """ Measure total runtime and returns it """
     start = time.perf_counter()
     await asyncio.gather(*(async_comprehension() for i in range(3)))
